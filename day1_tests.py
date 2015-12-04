58f48791f129fe2e0dbdd4de3c3063799ffbf88d
Mark Mollineaux <bufordsharkley@gmail.com>
2015-12-03 17:18:53 -0800

    after-the-fact TDD for day1

---
+++ day1_tests.py
@@ -0,0 +1,23 @@
+import unittest
+
+import day1
+
+class FloorTests(unittest.TestCase):
+
+    def test_expected_floor_reached(self):
+        self.assertEqual(day1.elevator('(())'), 0)
+        self.assertEqual(day1.elevator('()()'), 0)
+        self.assertEqual(day1.elevator('((('), 3)
+        self.assertEqual(day1.elevator('(()(()('), 3)
+        self.assertEqual(day1.elevator('))((((('), 3)
+        self.assertEqual(day1.elevator('())'), -1)
+        self.assertEqual(day1.elevator('))('), -1)
+        self.assertEqual(day1.elevator(')))'), -3)
+        self.assertEqual(day1.elevator(')())())'), -3)
+
+    def test_first_to_basement_agrees(self):
+        self.assertEqual(day1.first_to_basement(')'), 1)
+        self.assertEqual(day1.first_to_basement('()())'), 5)
+
+if __name__ == "__main__":
+    unittest.main()
