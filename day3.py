a2e6ae1164c6530cfe71b648f164fa4541c2fc9f
Mark Mollineaux <bufordsharkley@gmail.com>
2016-12-02 22:40:32 -0800

    Pointless change

--- day3.py
+++ day3.py
@@ -7,8 +7,8 @@ def group_into_trios(data):
     return (data[ii:ii + 3] for ii in range(0, len(data), 3))
 
 if __name__ == '__main__':
-    rows = open('input/3.txt').read().splitlines()
-    rows = [[int(x) for x in row.split()] for row in rows]
+    rows = [[int(x) for x in row.split()]
+            for row in open('input/3.txt').read().splitlines()]
     count_part_one = sum(1 for trio in rows if is_triangle(trio))
     print(count_part_one)
     # Flatten all the columns from the original input into one list, and group
