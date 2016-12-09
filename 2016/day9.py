98101e5ba5e31c3c25f21e624dd7d85a4e1757f8
Mark Mollineaux <bufordsharkley@gmail.com>
2016-12-09 01:06:34 -0800

    better

--- 2016/day9.py
+++ 2016/day9.py
@@ -1,4 +1,4 @@
-def find_length_one(text):
+def find_length(text, version=1):
     length = 0
     while text:
         try:
@@ -8,40 +8,20 @@ def find_length_one(text):
         length += len(token)
         marker, remaining = marker.split(')', 1)
         span, multiplier = tuple(int(x) for x in marker.split('x'))
-        length += span * multiplier
+        if version == 1:
+            length += span * multiplier
+        elif version == 2:
+            length += find_length(remaining[:span], version=2) * multiplier
         text = remaining[span:]
     length += len(text)
     return length
 
-
-def find_length_two(text):
-    length = 0
-    while text:
-        try:
-            token, marker = text.split('(', 1)
-        except ValueError:  # It's final token.
-            break
-        length += len(token)
-        marker, remaining = marker.split(')', 1)
-        span, multiplier = tuple(int(x) for x in marker.split('x'))
-        length += find_length_two(remaining[:span]) * multiplier
-        text = remaining[span:]
-    length += len(text)
-    return length
-
-
-def main():
-    assert find_length_one('ADVENT') == 6
-    assert find_length_one('A(1x5)BC') == 7
-    assert find_length_one('(3x3)XYZ') == 9
-    input_text = open('input/9.txt').read().strip()
-    answer_part_one = find_length_one(input_text)
-    print(answer_part_one)
-    assert answer_part_one == 107035
-    assert find_length_two('ADVENT') == 6
-    answer_part_two = find_length_two(input_text)
-    print(answer_part_two)
-    assert answer_part_two == 11451628995
 
 if __name__ == "__main__":
-    main()
+    input_text = open('input/9.txt').read().strip()
+    answer_part_one = find_length(input_text)
+    print(answer_part_one)
+    answer_part_two = find_length(input_text, version=2)
+    print(answer_part_two)
+    assert answer_part_one == 107035
+    assert answer_part_two == 11451628995
