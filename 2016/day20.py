692f21d3fb1267c9f0558dfb8e527baa54a7e9ca
Mark Mollineaux <bufordsharkley@gmail.com>
2016-12-20 13:52:46 -0800

    small

--- 2016/day20.py
+++ 2016/day20.py
@@ -8,20 +8,15 @@ def compressed(ranges):
       current = first, last
   yield current
 
-
-def main():
+if __name__ == "__main__":
     ranges = (x.strip() for x in open('input/20.txt').readlines())
     ranges = sorted([tuple(int(y) for y in x.split('-')) for x in ranges])
     lowest = 0
     valid_ips = []
     ranges = list(compressed(ranges))
-    for first, last in ranges:
+    for first, last in compressed(ranges):
       if lowest < first:
         valid_ips.extend(range(lowest, first))
       lowest = last + 1
     print valid_ips[0]
     print len(valid_ips)
-
-
-if __name__ == "__main__":
-    main()
