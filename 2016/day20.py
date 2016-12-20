42910e9809507df30382fd1a78dfd7dd3278a939
Mark Mollineaux <bufordsharkley@gmail.com>
2016-12-20 11:40:10 -0800

    Fix

--- 2016/day20.py
+++ 2016/day20.py
@@ -1,70 +1,27 @@
-def is_trap(*x):
-    assert len(x) == 3
-    return ''.join(x) in ('^^.', '.^^', '^..', '..^')
-
-
-def next_row(row):
-    resp = []
-    for ii, x in enumerate(row):
-        a = row[ii - 1] if ii > 0 else '.'
-        b = x
-        c = row[ii + 1] if ii < len(row) - 1 else '.'
-        resp.append('^' if is_trap(a, b, c) else '.')
-    return ''.join(resp)
-
-
-def safe_tiles_for_rows(numrows, row, count=0):
-    for _ in range(numrows):
-        count += sum(1 for x in row if x == '.')
-        row = next_row(row)
-    return count
-
-def compress(d):
-  resp = []
-  cur = None
-  for x in d:
-    if cur is not None:
-      if x[0] < cur[1]:
-        cur = cur[0], x[1]
-      else:
-        resp.append(cur)
-        cur = x
+def compressed(ranges):
+  current = ranges[0]
+  for first, last in ranges[1:]:
+    if first < current[1]:
+      current = current[0], max(last, current[1])
     else:
-      cur = x
-  resp.append(cur)
-  return resp
+      yield current
+      current = first, last
+  yield current
 
 
 def main():
-    d = [x.strip() for x in open('input/20.txt').readlines()]
-    d = [x.split('-') for x in d]
-    ranges = []
-    d  = sorted((int(x[0]), int(x[1])) for x in d)
+    ranges = (x.strip() for x in open('input/20.txt').readlines())
+    ranges = sorted([tuple(int(y) for y in x.split('-')) for x in ranges])
     lowest = 0
-    all_ = set()
-    for ii in d:
-      print len(all_)
-      for x in all_:
-        if ii[0] <= x <= ii[1]:
-          all_.remove(x)
-      if lowest < ii[0]:
-        for x in range(lowest, ii[0]):
-          all_.add(x)
-      lowest = ii[1] + 1
+    valid_ips = []
+    ranges = list(compressed(ranges))
+    for first, last in ranges:
+      if lowest < first:
+        valid_ips.extend(range(lowest, first))
+      lowest = last + 1
+    print valid_ips[0]
+    print len(valid_ips)
 
-    print all_
-    #ranges.append(range(int(x[0]), int(x[1])))
-    #Jkfor x in ranges:
-
-    return
-    assert safe_tiles_for_rows(3, '..^^.') == 6
-    assert safe_tiles_for_rows(10, '.^^.^.^^^^') == 38
-    assert safe_tiles_for_rows(10, '.^^.^.^^^^') == 38
-    part_one = safe_tiles_for_rows(40, x)
-    assert part_one == 1989
-    return
-    print part_one
-    print safe_tiles_for_rows(400000, x)
 
 if __name__ == "__main__":
     main()
